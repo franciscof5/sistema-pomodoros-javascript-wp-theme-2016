ab03aa73cb81e41c3b2b2175174d534e876a613b
Francisco Matelli <francisco@Franciscos-MacBook-Air.local>
2025-02-21 03:11:28 -0300

    years of private updates publiced

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
-<?php
-// Silence is golden.
+<?php 
+//NEW index.php, old is 404.php
+get_header("fullpage"); 
+
+wp_enqueue_script("fullpage-js");
+wp_enqueue_style("fullpage-css");
+wp_enqueue_style("fullPage_examples_examples.css");
+
+
+locate_template( "part-index.php", true );
+
+//get_footer();
+?>
